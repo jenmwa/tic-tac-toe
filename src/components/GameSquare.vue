d09d860298d0369830d017fd66b1cc6146ebbfb9
jenmwa <jenmwa@gmail.com>
2023-05-26 17:12:16 +0200

    testing out a gameSquare

---
+++ src/components/GameSquare.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <button class="square-btn">X or O</button>
+</template>
+
+<style scoped lang="scss">
+.square-btn {
+width: 10rem;
+height: 10rem;
+background: none;
+font-weight: 700;
+
+  &:hover {
+      cursor: pointer;
+    }
+
+  &:focus {
+    outline: none;
+    background-color: lightgrey;
+  }
+.clicked {
+  background-color: green;
+}
+}
+
+</style>
