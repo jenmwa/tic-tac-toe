8ca5de90108456aae68240341e3697e41a1e12f0
jenmwa <jenmwa@gmail.com>
2023-05-30 23:55:06 +0200

    class Player upd.

--- src/models/Player.ts
+++ src/models/Player.ts
@@ -2,8 +2,8 @@ export class Player {
   constructor(
     public name: string, 
     public userSymbol: string, //X or O
-    public active: boolean,
-    public winner: boolean,
-    public points: number,
-    ) {}
+    public active: boolean = false,
+    public winner: boolean = false,
+    public points: number = 0,
+  ) {}
 }
