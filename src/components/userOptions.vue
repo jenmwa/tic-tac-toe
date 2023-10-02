fea60e1111b9c29a937643684e0342e52b1c851b
jenmwa <jenmwa@gmail.com>
2023-10-02 20:10:48 +0200

    .

--- src/components/userOptions.vue
+++ src/components/userOptions.vue
@@ -1,37 +1,36 @@
-<script setup lang='ts'>
-import { ref } from 'vue';
-import { Player } from '../models/Player';
-import HighscoreComponent from './HighscoreComponent.vue'
+<script setup lang="ts">
+  import { ref } from "vue";
+  import { Player } from "../models/Player";
+  import HighscoreComponent from "./HighscoreComponent.vue";
 
-defineProps({
-  players: {
-    type: Array as () => Player[],
-  },
-});
+  defineProps({
+    players: {
+      type: Array as () => Player[],
+    },
+  });
 
-const emits = defineEmits<{
-  (e: 'reset') :void,
-  (e: 'newGame') :void;
-}>();
+  const emits = defineEmits<{
+    (e: "reset"): void;
+    (e: "newGame"): void;
+  }>();
 
-const newgameBtn = () => {  
-  emits('newGame');
-}
+  const newgameBtn = () => {
+    emits("newGame");
+  };
 
-const showHighscore = ref(false);
+  const showHighscore = ref(false);
 
-const highscoreBtn = () => {
-  showHighscore.value = true;
-}
+  const highscoreBtn = () => {
+    showHighscore.value = true;
+  };
 
-const closeHighscore = () => {
-  showHighscore.value = false;
-};
+  const closeHighscore = () => {
+    showHighscore.value = false;
+  };
 
-const resetBtn = () => {
-  emits('reset');
-}
-
+  const resetBtn = () => {
+    emits("reset");
+  };
 </script>
 
 <template>
@@ -40,26 +39,28 @@ const resetBtn = () => {
     <button @click="highscoreBtn">HIGHSCORE</button>
     <button @click="resetBtn">RESET</button>
   </div>
-  <HighscoreComponent v-if="showHighscore" @close="closeHighscore" :players="players"></HighscoreComponent>
+  <HighscoreComponent
+    v-if="showHighscore"
+    @close="closeHighscore"
+    :players="players"
+  ></HighscoreComponent>
 </template>
 
 <style scoped lang="scss">
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-block: 1rem;
+    margin-bottom: 7rem;
+  }
 
-div {
-  display:flex;
-  flex-direction: column;
-  align-items: center;
-  margin-block: 1rem;
-  margin-bottom: 7rem;
-}
-
-button {
-  width: 175px;
-  height: 35px;
-  border: 2px solid darkslategrey;
-  border-radius: 4px;
-  margin-bottom: 1rem;
-  cursor: pointer;
-}
-
+  button {
+    width: 175px;
+    height: 35px;
+    border: 2px solid darkslategrey;
+    border-radius: 4px;
+    margin-bottom: 1rem;
+    cursor: pointer;
+  }
 </style>
